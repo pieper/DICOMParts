7d91a28b92f80802809330ce49352597d8164bf5
Steve Pieper <pieper@isomics.com>
2023-10-25 16:30:56 -0400

    Update license and add some files

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from .databases import *
+from .indexers import *
+from .loaders import *
+from .adapters import *
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='dicomparts',
+    version='0.1.0',
+    description='A set of parts for working with DICOM',
+    author='Steve Pieper',
+    author_email='pieper@isomics.com',
+    url='https://github.com/pieper/DICOMParts',
+    packages=['dicomparts'],
+    install_requires=[
+        'pydicom',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    ],
+)
